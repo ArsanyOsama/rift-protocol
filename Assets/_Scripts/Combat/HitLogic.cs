391334e993f4a2a14556cb60471b3bc8926fdc27
Thomas Ayman <692400325@ecu.edu.eg>
2026-04-27 17:56:57 +0300

    fix collision logic and hit effects

--- Assets/_Scripts/Combat/HitLogic.cs
+++ Assets/_Scripts/Combat/HitLogic.cs
@@ -5,14 +5,34 @@ public class HitLogic : MonoBehaviour
     public GameObject hitSparkPrefab;
     public HitFlashController flashController;
 
-     void OnCollisionEnter(Collision collision)
+    private void OnCollisionEnter(Collision collision)
     {
-        ContactPoint contact = collision.contacts[0];
+        Rigidbody myRb = GetComponent<Rigidbody>();
+        Rigidbody otherRb = collision.gameObject.GetComponent<Rigidbody>();
 
-        // إظهار الشرارة
-        Instantiate(hitSparkPrefab, contact.point, Quaternion.identity);
+        if (otherRb != null)
+        {
+            // بنحسب سرعة كل واحدة فيهم وقت اللحظة دي
+            float mySpeed = myRb.velocity.magnitude;
+            float otherSpeed = otherRb.velocity.magnitude;
 
-        // تشغيل الوميض
-        if (flashController != null) flashController.StartFlash();
+            // لو سرعة اللي خبطني (otherSpeed) أكبر من سرعتي (mySpeed)
+            // ده معناه إنه هو اللي هاجمني، فـ أنا اللي هطلع الإيفيكت
+            if (otherSpeed > mySpeed)
+            {
+                if (hitSparkPrefab != null)
+                {
+                    ContactPoint contact = collision.contacts[0];
+                    Instantiate(hitSparkPrefab, contact.point, Quaternion.identity);
+                }
+
+                if (flashController != null)
+                {
+                    flashController.StartFlash();
+                }
+
+                Debug.Log(gameObject.name + " was hit by " + collision.gameObject.name);
+            }
+        }
     }
 }
